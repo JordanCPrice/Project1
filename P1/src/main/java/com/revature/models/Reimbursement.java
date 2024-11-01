c8566999761c76b223a318bf6d0ae4c844b00f1d
JordanCPrice <124534464+JordanCPrice@users.noreply.github.com>
2024-10-31 17:03:52 -0700

    Created base model objects

--- P1/src/main/java/com/revature/models/Reimbursement.java
+++ P1/src/main/java/com/revature/models/Reimbursement.java
@@ -1,4 +1,89 @@
 package com.revature.models;
 
+import jakarta.persistence.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+@Table(name ="reimbursements")
 public class Reimbursement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int reimbId;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private double amount;
+
+    @Column(nullable = false)
+    private String status;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "userId") // Links our FK to the PK from User
+    private User user;
+
+    public Reimbursement() {
+    }
+
+    public Reimbursement(int reimbId, String description, double amount, String status, User user) {
+        this.reimbId = reimbId;
+        this.description = description;
+        this.amount = amount;
+        this.status = status;
+        this.user = user;
+    }
+
+    public int getReimbId() {
+        return reimbId;
+    }
+
+    public void setReimbId(int reimbId) {
+        this.reimbId = reimbId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Reimbursement{" +
+                "reimbId=" + reimbId +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                ", status='" + status + '\'' +
+                ", user=" + user +
+                '}';
+    }
 }
