1d69fc4a297a77c4afca328ffdc0083f0eef948b
JordanCPrice <124534464+JordanCPrice@users.noreply.github.com>
2024-11-08 15:30:17 -0700

    Updated LoginRegister component

--- P1FrontEnd/p1-frontend/src/App.tsx
+++ P1FrontEnd/p1-frontend/src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Login } from './Components/LoginRegister/Login';
+import { Register } from './Components/LoginRegister/Register';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Login/>} /> {/* Blank path makes this component render at startup */}
+          <Route path="/register" element={<Register/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ P1FrontEnd/p1-frontend/src/Components/LoginRegister/Register.tsx
@@ -0,0 +1,97 @@
+import axios from "axios"
+import { useState } from "react"
+import { Button, Container, Form } from "react-bootstrap"
+import { useNavigate } from "react-router-dom"
+
+
+
+export const Register:React.FC = () => {
+
+    const navigate = useNavigate()
+
+
+    const [user, setUser] = useState({
+        username:"",
+        password:"",
+        firstName:"",
+        lastName:""
+    })
+
+    // Function to store values when an input box changes
+    const storeValues = (input:any) => {
+        
+        const name = input.target.name // the name of the input box that changed (username, password, firstName,lastName fields)
+        const value = input.value.name // the value in the input box
+
+        //input = the entire event (which got passed in as an argument)
+        //target = the specfic input box that triggereed the onChange event
+        // name/value = the name/ value of the box
+
+        // Take whatever input was changed, and set the matching field in user to the value in the field in the input
+        // The spread operator (...) allows us to access the values of the object individually
+        // [name] allows us to use this same line for username, password, first, and lastname
+        setUser((user) => ({...user, [name]:value}))
+        
+    }
+
+    // Function that sends the username/password to the backend
+    const register = async () => {
+
+        // POST request send to the backend
+        const response = await axios.post("http://localhost:7777/users", user)
+        
+        
+    }
+
+
+    return(
+
+        /* what's my and mx? this is margin for y and x axis
+        we set my-5 so that we have a decent amount of space away from the top of the page
+        mx-auto centers the content horizontally */
+        <Container className="my-5 mx-auto">
+            <div>
+                <h1>New here? Create an Account for free!</h1>
+
+                <div>
+                    <Form.Control
+                        type="text"
+                        placeholder="Username"
+                        name="username"
+                        onChange={storeValues}
+                    />
+                </div>
+                <div>
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        onChange={storeValues}
+                    />
+                </div>
+                <div>
+                    <Form.Control
+                        type="text"
+                        placeholder="First name"
+                        name="firstName"
+                        onChange={storeValues}
+                    />
+                </div>
+                <div>
+                    <Form.Control
+                        type="text"
+                        placeholder="Last name"
+                        name="lastName"
+                        onChange={storeValues}
+                    />
+                </div>
+
+                <div>
+                    <Button className="btn-success m-1" onClick={register}>Register</Button>
+                    <Button className="btn-dark" onClick={()=>navigate("/")}>Back</Button>
+                </div>
+            </div>
+        </Container>
+
+    )
+}
